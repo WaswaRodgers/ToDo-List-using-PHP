68a1571297dbdd8a8f0f3aa1bc60d36bf59db87f
Waswa Rodgers <waswam1994@gmail.com>
2020-07-15 22:03:30 +0300

    created database connection file

--- includes/database/connection.php
+++ includes/database/connection.php
@@ -3,10 +3,7 @@
 define('HOST', 'localhost');
 define('USER', 'root');
 define('PASSWORD', '');
-define('DATABASE', 'todolist');
+define('DATABASE', 'todolis');
 
-$conn = mysqli_connect("localhost", "root", "", "todolist");
+$conn = mysqli_connect(HOST, USER, PASSWORD, DATABASE) or die("Could not connect to database <font color='red'>".mysqli_connect_error()."</font>");
 
-if(!$conn) {
-    die("CONNECTION FAILED".mysqli_error($conn));
-}
